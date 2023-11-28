442e0d55a36e2db85296dd42ccc8c7b98fb2858a
vaibhavugale <kuldipw00@gmail.com>
2023-11-28 10:40:28 +0530

    update project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,19 +5,19 @@ import { Link } from 'react-router-dom'
 
 const ProjectCard = ({data}) => {
   return (
-    <div  className=' mt-4 shadow-2xl rounded-md w-[320px] min-h-[350px] mx-auto p-2 bg-white  hover:scale-90 transition-all duration-300 hover:bg-slate-200'>
-      {console.log(data?.Image)}
+    <div  className=' mt-4 shadow-2xl rounded-md w-[320px] min-h-[380px] mx-auto p-2 bg-white  hover:scale-90 transition-all duration-300 hover:bg-slate-200'>
+      {/* {console.log(data?.Image)} */}
       <img src={data?.Image} alt='Project image' className=' w-[300px] mx-auto mt-2 rounded-sm' />
 
       <div className=' mt-2 flex gap-y-1 flex-col'> 
-        <p className='   font-semibold'><span className=' font-extrabold'>Title:</span>{data?.name}</p>
+        <p className='   font-semibold'><span className=' font-extrabold'>{data?.name}</span></p>
 
          <div className=' flex  gap-y-0  gap-2 flex-wrap'>
            <h3 className=' font-semibold'> TecStack:</h3>
            
             
             {
-               data?.TecStack?.map((tec)=>(<p>{tec},</p>))
+               data?.TecStack?.map((tec)=>(<p className=' font-semibold'>{tec},</p>))
             }
 
          </div>
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const ProjectSection = () => {
  
   return (
-    <div id="project" className=" w-[1080px] mx-auto mt-[50px] p">
+    <div id="project" className=" w-[1080px] mx-auto mt-[50px] ">
       <div className="text-[3rem] text-gray-400 font-bold">Projects</div>
       <div className=" bg-blue-500 w-[80px]  h-[2px]"></div>
       <div className=" flex items-start gap-2 justify-start flex-wrap">
